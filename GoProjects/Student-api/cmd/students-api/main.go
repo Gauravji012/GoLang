4ae7b2ef633c3f745044cdfeef7c28766a435853
Gaurav Khandelwal <gauravkhandelwal327@gmail.com>
2025-10-27 12:26:29 +0530

    add git-ignore file content

--- GoProjects/Student-api/cmd/students-api/main.go
+++ GoProjects/Student-api/cmd/students-api/main.go
@@ -1,5 +1,8 @@
 package main
 //ye go ke ander entry point hota h. yha se kisi bhi project ka execcution start ho jata h
+//config file --> kisi bhi project ke liye configuration mgmt bhut jruri hota h kisi bhi project ke liye
+//we use file based config system
+
 import "fmt"
 
 func main() {
