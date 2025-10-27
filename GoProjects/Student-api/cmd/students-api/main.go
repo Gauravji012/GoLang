d02c2d9837fa65301c61b79f46ef38292e023b10
Gaurav Khandelwal <gauravkhandelwal327@gmail.com>
2025-10-27 12:13:03 +0530

    Initial commit

---
+++ GoProjects/Student-api/cmd/students-api/main.go
@@ -0,0 +1,7 @@
+package main
+//ye go ke ander entry point hota h. yha se kisi bhi project ka execcution start ho jata h
+import "fmt"
+
+func main() {
+ fmt.Println("Welcome to students api ...")
+}
---
+++ GoProjects/Student-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/Gauravji012/GoLang/GoProjects/Student-api
+
+go 1.25.2
